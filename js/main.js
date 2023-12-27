d1957f1271fe02dd6b87b2680a7af50e4edce074
Klaudia <koliber96k@gmail.com>
2023-12-27 18:17:38 +0100

    Projekt-strony-WoytasDesign

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// main.js
+
+
+alert("Niniejszy strona internetowa stosuje pliki cookies (tzw. ciasteczka). Informacja na temat celu ich przechowywania i sposobu zarządzania znajduje się w Polityce prywatności. Jeżeli nie wyrażasz zgody na zapisywanie informacji zawartych w plikach cookies - zmień ustawienia swojej przeglądarki.");
